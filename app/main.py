320837830f0a6b0a8fb1694c0ae3b19d2cef7a89
Preface_Ilja <gonciarov@yandex.ru>
2023-12-12 19:46:55 +0000

    app with no backend

--- app/main.py
+++ app/main.py
@@ -7,71 +7,7 @@ from authlib.integrations.starlette_client import OAuth, OAuthError
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
-app.add_middleware(SessionMiddleware, secret_key="add any string...")
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-oauth = OAuth()
-oauth.register(
-    name='google',
-    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
-    client_id='923647480819-2mqr12c7ecrvhlsincgjmihb69mp4rfr.apps.googleusercontent.com',
-    client_secret='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',
-    client_kwargs={
-        'scope': 'email openid profile',
-        'redirect_url': 'http://localhost:8000/auth/callback'
-    }
-)
-
-
-templates = Jinja2Templates(directory="templates")
-
 
 @app.get("/")
-def index(request: Request):
-    user = request.session.get('user')
-    if user:
-        return RedirectResponse('welcome')
-
-    return templates.TemplateResponse(
-        name="home.html",
-        context={"request": request}
-    )
-
-
-@app.get('/auth/welcome')
-def welcome(request: Request):
-    user = request.session.get('user')
-    print(user['picture'])
-    if not user:
-        return RedirectResponse('/')
-    return templates.TemplateResponse(
-        name='welcome.html',
-        context={'request': request, 'user': user}
-    )
-
-
-@app.get("/login")
-async def login(request: Request):
-    url = request.url_for('auth')
-    return await oauth.google.authorize_redirect(request, url)
-
-
-@app.get('/auth/callback')
-async def auth(request: Request):
-    try:
-        token = await oauth.google.authorize_access_token(request)
-    except OAuthError as e:
-        return templates.TemplateResponse(
-            name='error.html',
-            context={'request': request, 'error': e.error}
-        )
-    user = token.get('userinfo')
-    if user:
-        request.session['user'] = dict(user)
-    return RedirectResponse('welcome')
-
-
-@app.get('/logout')
-def logout(request: Request):
-    request.session.pop('user')
-    return RedirectResponse('/')
+def read_root():
+    return "Hello world"
